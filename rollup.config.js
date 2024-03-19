ae629e6b3a5fcddc1f14fdc6c355b601eff0591c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-03-19 08:39:54 -0400

    Add kg to safelist

--- rollup.config.js
+++ rollup.config.js
@@ -48,7 +48,7 @@ export default defineConfig({
       plugins: [
         purgecss({
           content: ["./**/*.hbs"],
-          safelist: [/^gh-/],
+          safelist: [/^gh-/, /^kg-/],
         }),
       ],
     }),
